3c8d8746b4807dfa892239cbf3aea77f37629f3e
Lucas Rodrigues <lucasrodrigues.mr@gmail.com>
2022-08-30 21:51:45 -0300

    Refactor: Modal - Padrão '$' nas variáveis

--- script.js
+++ script.js
@@ -4,21 +4,21 @@
     })
 
     const modal = () => {
-        const btnOpenModal = document.querySelector('.btn-modal')
-        const btnCloseModal = document.querySelector('.btn-close') 
-        const modal = document.querySelector('.modal-box')
+        const $btnOpenModal = document.querySelector('.btn-modal')
+        const $btnCloseModal = document.querySelector('.btn-close') 
+        const $modal = document.querySelector('.modal-box')
 
-        btnOpenModal.addEventListener('click', openModal)
-        btnCloseModal.addEventListener('click', closeModal)
+        $btnOpenModal.addEventListener('click', openModal)
+        $btnCloseModal.addEventListener('click', closeModal)
 
         function openModal() {
-            modal.classList.add('active')
-            btnOpenModal.style.display = 'none'
+            $modal.classList.add('active')
+            $btnOpenModal.style.display = 'none'
         }
 
         function closeModal() {
-            modal.classList.remove('active')
-            btnOpenModal.style.display = 'initial'
+            $modal.classList.remove('active')
+            $btnOpenModal.style.display = 'initial'
         }
     }
 })()
